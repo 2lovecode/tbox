2f281d002642249bc9e9e110fcfde5e69149124e
2lovecode <liu546hao@163.com>
2025-03-26 12:47:02 +0800

    package

---
+++ resources/packaging/macos/wrapper.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+osascript -e 'do shell script "/*/Tbox.app/Contents/MacOS/tbox >/dev/null 2>&1 &" with prompt "Comfortably monitor your Internet traffic." with administrator privileges'
